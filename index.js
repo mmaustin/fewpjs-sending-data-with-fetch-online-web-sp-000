694e7976109528c8a1c9d1b8335969557100c6a5
McCray <tubach75@hotmail.com>
2021-06-07 10:43:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(userName, userEmail){
+
+const dataObject = {
+    name: userName,
+    email: userEmail
+}
+
+const configObj = {
+    method: 'POST',
+    headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    },
+    body: JSON.stringify(dataObject)
+}
+
+return fetch('http://localhost:3000/users', configObj)
+
+    .then(function(response){
+        return response.json();
+    })
+        .then(function(object){
+            let b = document.querySelector('body');
+            b.innerHTML = object.id;
+        })
+            .catch(function(error){
+                let b = document.querySelector('body');
+                b.innerHTML = error.message;
+            })
+    
+}
+submitData("Mark", "mark@email.com")
